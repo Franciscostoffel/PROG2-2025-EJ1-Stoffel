49f6e23c0d624160229f772ac1a03bddcc02c6e4
francisco stoffel <stoffelfrancisco@gmail.com>
2025-07-28 21:13:33 -0300

    .

--- EjercicioEntregable/src/main/java/org/example/Main.java
+++ EjercicioEntregable/src/main/java/org/example/Main.java
@@ -1,14 +1,9 @@
 package org.example;
 
-import org.example.dto.CuentaDTO;
-import org.example.entidades.CajaDeAhorro;
-import org.example.entidades.CuentaCorriente;
 import org.example.envios.AirShippingStrategy;
 import org.example.envios.ShippingStrategy;
 import org.example.envios.TruckShippingStrategy;
-import org.example.pagos.*;
 import org.example.service.EnvioLogica;
-import org.example.service.LogicaCuenta;
 import org.example.service.PagoManager;
 
 import java.util.concurrent.CompletableFuture;
@@ -16,31 +11,6 @@ import java.util.concurrent.CompletableFuture;
 public class Main {
     public static void main(String[] args) {
         try {
-            LogicaCuenta logica = LogicaCuenta.getInstancia();
-            // Crear cuentas con builders internos
-            CajaDeAhorro caja = new CajaDeAhorro.Builder()
-                    .setId(1)
-                    .setSaldoInicial(1000)
-                    .build();
-
-            CuentaCorriente corriente = new CuentaCorriente.Builder()
-                    .setId(2)
-                    .setSaldoInicial(500)
-                    .setLimiteDescubierto(300)
-                    .build();
-
-            logica.agregarCuenta(caja);
-            logica.agregarCuenta(corriente);
-
-            logica.agregarSaldo(1, 500);       // CajaDeAhorro: 1500
-            logica.quitarSaldo(2, 700);        // CuentaCorriente: queda en -200
-
-            CuentaDTO dto1 = logica.obtenerCuentaDTO(1);
-            CuentaDTO dto2 = logica.obtenerCuentaDTO(2);
-
-            System.out.println("Cuenta 1 - Tipo: " + dto1.getTipo() + ", Saldo: " + dto1.getSaldo());
-            System.out.println("Cuenta 2 - Tipo: " + dto2.getTipo() + ", Saldo: " + dto2.getSaldo());
-
             // Crear estrategias de envío
             ShippingStrategy envioAvion = new AirShippingStrategy();
             ShippingStrategy envioCamion = new TruckShippingStrategy();
@@ -79,5 +49,3 @@ public class Main {
         }
     }
 }
-
-//jgfkhgfhtgf
